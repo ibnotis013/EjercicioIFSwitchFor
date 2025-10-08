1a3ec7fbff05eb099014dc18963fad00394c83b8
ibnotis013 <n.ledo@liceolapaz.net>
2025-10-08 09:22:26 +0200

    ejercicio 1 terminado

--- src/ejercicio1/pincipal.java
+++ src/ejercicio1/pincipal.java
@@ -1,7 +1,21 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class pincipal {
     static void main() {
-        
+        System.out.println("introduce la velocidad del vehiculo (numero entero)");
+        int velocidad;
+        Scanner sc = new Scanner(System.in);
+        velocidad = sc.nextInt();
+        boolean esExcesoVelocidad = true;
+        if (velocidad >120) {
+            System.out.println("¡Atención! Vas a una velocidad excesiva. ¡Riesgo de multa! ");
+        }else {
+            System.out.println("velocidad adecuada, siga asi y conduzca con seguridad");
+        }
+
+
+
     }
 }
